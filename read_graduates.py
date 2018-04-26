8815a05a80c23cba3e71378051545efec019d1ea
Trevor Cohen <tecohen@umail.ucsb.edu>
2018-04-26 13:42:25 -0700

    possible changes after looking at Catrina code

---
+++ read_graduates.py
@@ -0,0 +1,27 @@
+import json
+import random
+
+# This code loads data from data_in.json
+# into a Python dictionary called d
+
+with open('graduates.json') as json_data:
+    graduate = json.load(json_data)
+
+#shorterList = graduate[0:3]
+def MajorInformation (c):
+    return c['Major Information']=='PETROLEUM ENGINEERING'
+
+onlyMajorInformation=[]
+for c in major:
+    if from 'Petroleum ENGINEERING'(c):
+        onlyMajorInformation.append
+
+with open('graduates.json', 'w') as outfile:
+    json.dump(onlyMajorInformation, outfile)
+radomshow=random.sample (onlyMajorInformaion,3)
+print(onlyMajorInformation)
+
+for show in randomshow:
+    print ('{:10} {:25}'.format(show['Show']['Name'][0:10], show['Show']['Type']
+    print (show['Show']['Name'],show['Show']['Type'])
+                                
